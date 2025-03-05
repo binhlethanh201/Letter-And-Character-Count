14f3d59aff99749c2d4fe6cc71aed1d603b1aff4
binhlethanh201 <ltbinh04@gmail.com>
2025-03-05 20:24:50 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,34 +1,39 @@
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		//input the content
-		System.out.println("Enter your content: ");
-		String input = sc.nextLine().trim().toLowerCase();
-		// count words
-		StringTokenizer token = new StringTokenizer(input, " ,./\\!?'';:[]{}");
+	
+	private static final Scanner sc = new Scanner(System.in);
+	
+	// count words
+	private static Map<String, Integer> wordCount(String input){
+		StringTokenizer token = new StringTokenizer(input, " ,./\\!?'';:[]{}?!@#$%^&*()");
 		Map<String, Integer> wordCount = new LinkedHashMap<>();
 		while(token.hasMoreTokens()) {
 			String word = token.nextToken();
 			wordCount.put(word, wordCount.getOrDefault(word, 0) +1);
 		}
-		//count letters
+		return wordCount;
+	}
+	
+	//count letters
+	private static Map<Character, Integer> letterCount(String input){
 		Map<Character, Integer> letterCount = new LinkedHashMap<>();
 		for(char c : input.toCharArray()) {
 			if(Character.isLetter(c)) {
 				letterCount.put(c, letterCount.getOrDefault(c, 0) + 1);
 			}
 		}
+		return letterCount;
+	}
+	
+	public static void main(String[] args) {
 		
+		//input the content
+		System.out.println("Enter your content: ");
+		String input = sc.nextLine().trim().toLowerCase();
 		//Print out the result
-		System.out.println(wordCount);
-		System.out.println(letterCount);
-		sc.close();
+		System.out.println(wordCount(input));
+		System.out.println(letterCount(input));
 
 	}
 
